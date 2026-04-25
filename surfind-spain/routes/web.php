fa2ed1a903f455aa12b1b2e6b6e4ecdd7ffeb319
dsotogc <davidsotgar3@gmail.com>
2026-04-25 10:44:24 +0200

    feat: create admin routes

--- surfind-spain/routes/web.php
+++ surfind-spain/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\BeachController as AdminBeachController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +12,28 @@ Route::view('comunidad', 'community.index')->name('community.index');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::prefix('admin')->as('admin.')->middleware('role:admin')->group(function () {
+        Route::group(['prefix' => 'playas', 'as' => 'beaches.', 'controller' => AdminBeachController::class], function () {
+            Route::get('/', 'index')->name('index')->middleware('permission:view beaches');
+            Route::get('/create', 'create')->name('create')->middleware('permission:create beaches');
+            Route::post('/store', 'store')->name('store')->middleware('permission:create beaches');
+            Route::get('/{beach}', 'show')->name('show')->middleware('permission:view beaches');
+            Route::get('/{beach}/edit', 'edit')->name('edit')->middleware('permission:edit beaches');
+            Route::put('/{beach}/edit', 'update')->name('update')->middleware('permission:edit beaches');
+            Route::delete('/{beach}/destroy', 'destroy')->name('destroy')->middleware('permission:delete beaches');
+        });
+
+        Route::group(['prefix' => 'usuarios', 'as' => 'users.', 'controller' => AdminUserController::class], function () {
+            Route::get('/', 'index')->name('index')->middleware('permission:manage users');
+            Route::get('/create', 'create')->name('create')->middleware('permission:manage users');
+            Route::post('/store', 'store')->name('store')->middleware('permission:manage users');
+            Route::get('/{user}', 'show')->name('show')->middleware('permission:manage users');
+            Route::get('/{user}/edit', 'edit')->name('edit')->middleware('permission:manage users');
+            Route::put('/{user}/edit', 'update')->name('update')->middleware('permission:manage users');
+            Route::delete('/{user}/destroy', 'destroy')->name('destroy')->middleware('permission:manage users');
+        });
+    });
 });
 
 require __DIR__.'/settings.php';
